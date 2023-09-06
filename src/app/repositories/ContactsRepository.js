70895437da50368fbb359d117a382598d3866a47
JhonataFigueredo <jhowfigueredo2@gmail.com>
2023-09-06 16:30:44 -0300

    feat: route created to delete contact

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,6 +1,6 @@
 const { v4: uuid } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: uuid(),
     name: 'John Doe',
@@ -8,11 +8,28 @@ const contacts = [
     phone: '555-555-5555',
     category_id: uuid(),
   },
+  {
+    id: uuid(),
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    phone: '111-222-5555',
+    category_id: uuid(),
+  },
 ];
 
 class ContactsRepository {
   findAll() {
     return Promise.resolve(contacts);
+  }
+
+  findById(id) {
+    const contact = contacts.find((item) => item.id === id);
+    return Promise.resolve(contact);
+  }
+
+  delete(id) {
+    contacts = contacts.filter((item) => item.id !== id);
+    return Promise.resolve();
   }
 }
 
