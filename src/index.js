265e51ea23506340549177201dae42896382ade5
JhonataFigueredo <jhowfigueredo2@gmail.com>
2023-09-11 10:47:07 -0300

    feat: implement async error handling with express-async-errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+require('express-async-errors');
 
-const router = require('./app/routes');
+const routes = require('./app/routes');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(router);
+app.use(routes);
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, request, response, next) => {
+  response.sendStatus(500);
+});
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
